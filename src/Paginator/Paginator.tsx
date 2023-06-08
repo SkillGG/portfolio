41e5788be49500033ede886f5150634370779b1f
SkillGG <skilletssgames@gmail.com>
2023-06-08 22:38:29 +0200

    Remove uunecesarry comments and code

--- src/Paginator/Paginator.tsx
+++ src/Paginator/Paginator.tsx
@@ -3,6 +3,8 @@ import { PAGES } from "../App";
 import Main from "./Main/Main";
 
 import "./Paginator.css";
+import About from "./About/About";
+import Projects from "./Projects/Projects";
 
 interface PaginatorProps {
     page: PAGES;
@@ -14,13 +16,13 @@ const Paginator: FunctionComponent<PaginatorProps> = ({ page }) => {
             {(() => {
                 switch (page) {
                     case PAGES.ABOUT:
-                        return <>about</>;
+                        return <About />;
                     case PAGES.MAIN:
                         return <Main />;
                     case PAGES.PROJECTS:
-                        return <>projects</>;
+                        return <Projects />;
                     default:
-                        return <>Oops</>;
+                        return <>This page doesn't exist!</>;
                 }
             })()}
         </main>
